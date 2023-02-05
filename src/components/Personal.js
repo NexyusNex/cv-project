7f970c01a988621ed43c552856200f411910f3b6
NexyusNex <shootdietale@gmail.com>
2023-02-05 17:21:37 +0100

    Added styling for personal details

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -4,22 +4,30 @@ class Personal extends React.Component {
   render() {
     return (
       <div className="personal-input">
-        <label for="Fname">
-          First name:
-          <input type="text" id="Fname" name="Fname"></input>
-        </label>
-        <label for="Lname">
-          Last name:
-          <input type="text" id="Lname" name="Lname"></input>
-        </label>
-        <label for="email">
-          E-mail:
-          <input type="email" id="email" name="email"></input>
-        </label>
-        <label for="tel">
-          Phone number:
-          <input type="tel" id="tel" name="tel"></input>
-        </label>
+        <h2>Personal details:</h2>
+        <label htmlFor="Fname">First name:</label>
+        <input
+          type="text"
+          id="Fname"
+          name="Fname"
+          placeholder="First name"
+        ></input>
+        <label htmlFor="Lname">Last name:</label>
+        <input
+          type="text"
+          id="Lname"
+          name="Lname"
+          placeholder="Last name"
+        ></input>
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="John123@gmail.com"
+        ></input>
+        <label htmlFor="tel">Phone number:</label>
+        <input type="tel" id="tel" name="tel" placeholder="0612356163"></input>
       </div>
     );
   }
