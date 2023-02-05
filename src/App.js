a37b6be70cea128d8ea20ae5c8b0c4da0983672b
NexyusNex <shootdietale@gmail.com>
2023-02-05 15:25:10 +0100

    Added a basic layout and styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <div className="input-container">
+          <h1>CV Application</h1>
+        </div>
+        <div className="info-container">
+          <h1>Info:</h1>
+        </div>
+      </div>
+    );
   }
 }
 
