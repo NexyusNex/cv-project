6b4fa03644438f7bc5789554efb852548f152cb7
NexyusNex <shootdietale@gmail.com>
2023-02-06 18:34:13 +0100

    Inputs now get displayed on click

--- src/App.js
+++ src/App.js
@@ -2,16 +2,90 @@ import React from "react";
 import Personal from "./components/Personal";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Fname: "",
+      Lname: "",
+      CV: {
+        Fname: "",
+        Lname: "",
+        email: "",
+        tel: "",
+      },
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.ApplyInfo = this.ApplyInfo.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  ApplyInfo(e) {
+    e.preventDefault();
+    this.setState({
+      CV: {
+        Fname: this.state.Fname,
+        Lname: this.state.Lname,
+        email: this.state.email,
+        tel: this.state.tel,
+      },
+    });
+  }
+
   render() {
+    const { CV } = this.state;
+    console.log(this.state.Fname);
     return (
-      <form className="container">
+      <form className="container" onSubmit={this.ApplyInfo}>
         <div className="input-container">
           <h1>CV Application</h1>
-          <Personal></Personal>
+
+          <div className="personal-input">
+            <h2>Personal details:</h2>
+            <label htmlFor="Fname">First name:</label>
+            <input
+              type="text"
+              id="Fname"
+              name="Fname"
+              placeholder="First name"
+              onChange={this.handleChange}
+            ></input>
+            <label htmlFor="Lname">Last name:</label>
+            <input
+              type="text"
+              id="Lname"
+              name="Lname"
+              placeholder="Last name"
+              onChange={this.handleChange}
+            ></input>
+            <label htmlFor="email">E-mail:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="John123@gmail.com"
+              onChange={this.handleChange}
+            ></input>
+            <label htmlFor="tel">Phone number:</label>
+            <input
+              type="tel"
+              id="tel"
+              name="tel"
+              placeholder="0612356163"
+              onChange={this.handleChange}
+            ></input>
+          </div>
           <button>Submit</button>
         </div>
         <div className="info-container">
           <h1>Info:</h1>
+          <Personal CV={CV}></Personal>
         </div>
       </form>
     );
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,33 +1,17 @@
 import React from "react";
 
 class Personal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div className="personal-input">
-        <h2>Personal details:</h2>
-        <label htmlFor="Fname">First name:</label>
-        <input
-          type="text"
-          id="Fname"
-          name="Fname"
-          placeholder="First name"
-        ></input>
-        <label htmlFor="Lname">Last name:</label>
-        <input
-          type="text"
-          id="Lname"
-          name="Lname"
-          placeholder="Last name"
-        ></input>
-        <label htmlFor="email">E-mail:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="John123@gmail.com"
-        ></input>
-        <label htmlFor="tel">Phone number:</label>
-        <input type="tel" id="tel" name="tel" placeholder="0612356163"></input>
+      <div>
+        <p>{this.props.CV.Fname}</p>
+        <p>{this.props.CV.Lname}</p>
+        <p>{this.props.CV.email}</p>
+        <p>{this.props.CV.tel}</p>
       </div>
     );
   }
